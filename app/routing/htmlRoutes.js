ee6fcf3d63efa189efa6ba499bf51c301fa1106c
Mahi Mani <m.mahishamani@gmail.com>
2019-10-03 23:59:08 -0700

    coded to get user input and store as array and display in api/friends route

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,3 +1,30 @@
-app.get("/survey", function(req, res){
-    res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+// On click of submit button
+$("#submit-btn").on("click", function(event){
+    // To prevent page from loading
+    event.preventDefault();
+
+    var name = $("#enterName").val().trim();
+    var email = $("#enterEmail").val().trim();
+    var answerArr = [];
+
+    for(var i=1; i<=50; i++){
+    if($("input[id=optionsRadios"+i+"]:checked").val()){
+        answerArr.push($("input[id=optionsRadios"+i+"]:checked").val());
+    }
+    
+}
+var newFriend = {
+        name: name,
+        email: email,
+        score: answerArr
+      }
+
+      $.post("/api/friends", newFriend)
+        .then(function(data) {
+          console.log(data);
+          alert("Finding your match..");
+        });
+
+    
+
 })
--- server.js
+++ server.js
@@ -9,14 +9,29 @@ var PORT = process.env.PORT || 3000;
 // variable declarations
 var friends = [
     {
-        name: "Mahisha",
+        name: "Joey",
         photo: "",
-        scores: [5, 4, 3, 6, 7, 2, 1, 3, 4, 9]
+        scores: [5, 1, 4, 1, 1, 1, 4, 1, 5, 1]
     },
     {
-        name: "Manikandan",
+        name: "Phoebe",
         photo: "",
-        scores: [3, 2, 5, 7, 6, 2, 1, 3, 4, 9]
+        scores: [5, 3, 4, 1, 1, 4, 5, 2, 1, 4]
+    },
+    {
+        name: "Monica",
+        photo: "",
+        scores: [3, 4, 1, 5, 5, 5, 1, 3, 1, 3]
+    },
+    {
+        name: "Rachel",
+        photo: "",
+        scores: [3, 5, 5, 3, 3, 1, 2, 1, 1, 2]
+    },
+    {
+        name: "Ross",
+        photo: "",
+        scores: [1, 2, 2, 3, 1, 1, 3, 3, 1, 5]
     }
 ]
 // Sets up the Express app to handle data parsing
@@ -34,6 +49,15 @@ app.get("/api/friends", function(req, res){
 app.get("/survey", function(req, res){
     res.sendFile(path.join(__dirname, "/app/public/survey.html"));
 })
+
+app.post("/api/friends", function(req, res) {
+    var newFriend = req.body;
+
+    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+    console.log(newFriend);
+    friends.push(newFriend);
+    res.json(newFriend);
+});
 
 app.listen(PORT, function(){
     console.log("App listening to PORT " + PORT);
